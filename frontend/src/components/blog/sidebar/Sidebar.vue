35ab72bd5fc0c49bdc4ab15e8eafed60e6a1b252
제유빈 <yubinje630@gmail.com>
2020-07-22 16:52:36 +0900

    S03P12A306-18, S03P12A306-20 | InProgress | Blog Structure, Blog Main Page

---
+++ frontend/src/components/blog/sidebar/Sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container bg-light">
+    <router-link :to="{ name: 'Blog' }">Blog Home</router-link>
+    <div v-for="category in categories" :key="category">
+      <router-link :to="{ name: 'BlogCategory' }">{{ category }}</router-link>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  components: {
+    
+  },
+  // props: {
+  //   categories: {
+  //     type: String,
+  //   }
+  // }
+  data() {
+    return {
+      categories: ['category1', 'category2']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
