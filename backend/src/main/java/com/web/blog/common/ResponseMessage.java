722561ab07b1067ab8d163c3bae5c4ea892a0d41
TAEK <hyungtaik@naver.com>
2020-07-23 10:11:44 +0900

    S03P12A306-8 | Done | API Document Complete

--- backend/src/main/java/com/web/blog/common/ResponseMessage.java
+++ backend/src/main/java/com/web/blog/common/ResponseMessage.java
@@ -3,6 +3,8 @@ public class ResponseMessage {
 
     public static final String LOGIN_SUCCESS = "로그인 성공";
     public static final String LOGIN_FAIL = "로그인 실패";
+    public static final String LOGOUT_SUCCESS = "로그아웃 성공";
+    public static final String LOGOUT_FAIL = "로그아웃 실패";
 
     public static final String READ_USER = "회원 정보 조회 성공";
     public static final String NOT_FOUND_USER = "회원을 찾을 수 없습니다.";
