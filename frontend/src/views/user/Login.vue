0ad55e9513f5149499f44f4188c79005efc5e4b9
김재영 <gjr191@naver.com>
2020-07-23 09:53:56 +0900

    S03P12A306-17| In Progress | Apply Vuex

--- frontend/src/views/user/Login.vue
+++ frontend/src/views/user/Login.vue
@@ -2,9 +2,9 @@
   <div>
     <b-modal id="login-modal" title="Login" hide-footer="true" >
     <h1>로그인</h1>
-    <input type="text" id="email" placeholder="이메일 입력">
+    <input v-model="loginData.email" type="text" id="email" placeholder="이메일 입력">
     <br>
-    <input type="password" id="password" placeholder="비밀번호 입력">
+    <input v-model="loginData.password" type="password" id="password" placeholder="비밀번호 입력">
     <br>
     <a href="">비밀번호 재설정</a>
     <br>
@@ -19,8 +19,22 @@
 </template>
 
 <script>
+
+import { mapActions } from 'vuex'
+
 export default {
   name: 'Login',
+  data() {
+    return {
+      loginData: {
+        email: null,
+        password: null
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['login'])
+  }
 }
 </script>
 
