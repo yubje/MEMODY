015cd36a56eb6db4143a3589463744e6c2607b2e
TAEK <hyungtaik@naver.com>
2020-07-23 16:34:52 +0900

    S03P12A306-52 | Done | Docker Apply & Test

--- backend/src/main/java/com/web/blog/config/security/UsersRepository.java
+++ backend/src/main/java/com/web/blog/config/security/UsersRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
-    Optional<User> findByEmail(String email);
+    Optional<Users> findByEmail(String email);
 }
