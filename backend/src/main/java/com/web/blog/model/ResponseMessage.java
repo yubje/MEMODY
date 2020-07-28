f42262773f3ce9d522139a8ceb198ee5b0e46251
TAEK <hyungtaik@naver.com>
2020-07-28 16:55:40 +0900

    S03P12A306-75 | Done | Main Before & After

--- backend/src/main/java/com/web/blog/model/ResponseMessage.java
+++ backend/src/main/java/com/web/blog/model/ResponseMessage.java
@@ -24,8 +24,15 @@ public class ResponseMessage {
     // Blog
     public static final String CREATE_BLOG_SUCCESS = "블로그 생성 성공";
     public static final String CREATE_BLOG_FAIL = "블로그 생성 실패";
-    public static final String SEARCH_MYBLOG_NONE = "블로그가 없습니다.";
-    public static final String SEARCH_MYBLOG_SUCCESS = "블로그 조회 성공";
+    public static final String SEARCH_MYBLOG_NONE = "내 블로그가 없습니다.";
+    public static final String SEARCH_MYBLOG_SUCCESS = "내 블로그가 조회 성공.";
+    public static final String SEARCH_BLOG_SUCCESS = "블로그 조회 성공";
+    public static final String SEARCH_BLOG_FAIL = "블로그 조회 실패";
+    
+    public static final String RECOMMEND_BLOG_SUCCESS = "추천 블로그 목록 조회 성공";
+    public static final String MAIN_SUCCESS = "메인 페이지 성공";
+    
+    
     
 
     public static final String AUTHORIZED = "인증 성공";
