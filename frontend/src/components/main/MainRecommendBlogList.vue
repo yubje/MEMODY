c620c1dcd75d4e7704df66dac3622cd69f8db550
yubje <yubinje630@gmail.com>
2020-07-31 02:07:55 +0900

    [MR] | 2020.07.31 | 'featureFront/design' into 'front'

--- frontend/src/components/main/MainRecommendBlogList.vue
+++ frontend/src/components/main/MainRecommendBlogList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>추천 블로그</h3>
-    <div style="border:1px solid;  width:85%; margin:auto" class="container-fluid">
+    <div style="width:85%; margin:auto" class="container-fluid">
       <div class="row">
         <MainMyBlogItem class="col-md-4 mt-2" v-for="myblog in recommendBlog" :key="myblog.bid" :myblog="myblog"/>
       </div>
