aa4ab9522b82d4a40a842608e08e24ac921cda1d
현유진 <jin961221@naver.com>
2020-07-30 22:31:34 +0900

    S03P12A306-19 | Done | Make Main Page Frame

--- frontend/src/services/blog-service.js
+++ frontend/src/services/blog-service.js
@@ -12,7 +12,7 @@ class BlogService {
     axios.post(`${SERVER}/blogs`, state.newBlogData, {headers: {"auth": cookies.get('auth-token')}})
       .then(() => {
         commit('CLEAR_NEWBLOGDATA')
-        router.push({ name: 'Main'})
+        router.push({ name: 'Main'}).catch((error)=>console.log(error))
       })
       .catch(error => console.log(error.response.data.message))
   }
