77e9862604b7e7751966bd8a841dce1a338150b4
제유빈 <yubinje630@gmail.com>
2020-08-07 22:49:28 +0900

    S03P13A306-56 | In Progress | Front / Add Profile Image

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,6 +14,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import VueCookies from 'vue-cookies'
 
+
 Vue.use(VueCookies)
 
 // Install BootstrapVue
