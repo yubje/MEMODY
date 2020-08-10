af42017b115b007b67701171c609aab616a68fa0
jkim0715 <gjr191@naver.com>
2020-08-10 16:55:20 +0900

    S03P13A306-70 | In Progress | Apply Dialogs

--- frontend/src/views/user/UserLoginView.vue
+++ frontend/src/views/user/UserLoginView.vue
@@ -3,66 +3,62 @@
 
     <v-row justify="center">
 
-    <v-dialog
-      v-model="dialog"
-      max-width="290"
-    >
-      <v-card>
-        <v-card-title class="headline">로그인</v-card-title>
+      <v-dialog v-model="dialog" persistent max-width="290">
+        <v-card>
+          <v-card-title class="headline">로그인</v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field v-model="loginData.email" label="이메일" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field v-model="loginData.password" label="비밀번호" type="password" required></v-text-field>
+                </v-col>
+              </v-row>
+              <div>
+                <router-link :to="{ name: 'UserResetPWCheckEmailView' }" data-dismiss="modal" data-toggle="modal"
+                  data-target="#resetpwcheckemail-modal">비밀번호 찾기</router-link>
+              </div>
 
-        <v-card-text>
-          <v-container v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field  v-model="loginData.email" label="이메일" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field  v-model="loginData.password" label="비밀번호" required></v-text-field>
-              </v-col>
-            </v-row>
-            <router-link :to="{ name: 'UserResetPWCheckEmailView' }" data-dismiss="modal" data-toggle="modal" data-target="#resetpwcheckemail-modal" >비밀번호 찾기</router-link>
-
-          </v-container>
-          <a>아직 회원이 아니세요? </a>
-          <router-link :to="{ name: 'UserSignupView' }" data-dismiss="modal" data-toggle="modal" data-target="#signup-modal" >회원 가입</router-link>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="green darken-1"
-            text
-            @click="login(loginData)"
-          >
-            로그인
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+              <span>아직 회원이 아니세요? </span>
+              <router-link :to="{ name: 'UserSignupView' }" data-dismiss="modal" data-toggle="modal"
+                data-target="#signup-modal">회원 가입</router-link>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="login(loginData)">
+              로그인
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
   </div>
 
 </template>
 
 <script>
+  import {
+    mapActions
+  } from 'vuex'
 
-import { mapActions } from 'vuex'
-
-export default {
-  name: 'UserLoginView',
-  data() {
-    return {
-      dialog: true,
-      loginData: {
-        email: null,
-        password: null
+  export default {
+    name: 'UserLoginView',
+    data() {
+      return {
+        dialog: true,
+        loginData: {
+          email: null,
+          password: null
+        }
       }
+    },
+    methods: {
+      ...mapActions(['login'])
     }
-  },
-  methods: {
-    ...mapActions(['login'])
   }
-}
 </script>
 
 <style>
