103f40fe1617d60271a7a42f0cc778a3e61403d9
minkyungcho <xiuperjjo@naver.com>
2020-08-18 18:00:00 +0900

    S03P13A306-131 | InProgress | Post Design

--- frontend/src/components/blog/post/BlogPostDetail.vue
+++ frontend/src/components/blog/post/BlogPostDetail.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <BlogPostSidebar/>     
       <div class="col">
-        <v-card-title>{{ postData.ptitle }}</v-card-title>
+        <v-card-title style="font-size: 1.5rem; font-weight: 1000;">{{ postData.ptitle }}</v-card-title>
         <ul v-if="userInfo.email == postData.manager | userInfo.roles[0] === 'ROLE_ADMIN'" style="float: right">
           <li class="nav-item dropdown">
           <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -23,9 +23,16 @@
           </div>
         </li>
         </ul>
-        <p style="text-align: left; margin-bottom: 0px">관리자: {{ postData.manager }}</p>
-        <p style="text-align: left; margin-bottom: 0px">원작자: {{ postData.author }}</p>
-        <p style="text-align: left; margin-bottom: 0px">작성일 {{ postData.postTime.slice(0,10) }}  | 수정일 {{ postData.updateTime.slice(0,10) }}</p>
+        <div style="margin-bottom: 10px">
+          <p style="text-align: left; margin-bottom: 3px;">
+            <img v-if="postData.managerProfile" id="profile-img-small" :src="postData.managerProfile">
+            <img v-else id="profile-img-small" src="@/assets/img/user-default.png">
+            <span>{{ postData.managerNickname }}  </span>
+            <span v-if="time.includes('전') == false" style="color: gray; font-size:13px;"> {{ $moment(time).format('YYYY.MM.DD HH:mm') }}</span>
+            <span v-else style="color: gray; font-size:13px;"> {{ time }}</span>
+          </p>
+          <p v-if="postData.managerNickname != postData.authorNickname" style="text-align: left; margin-left: 16px; margin-bottom: 0px; font-size:14px;"><span style="font-weight: bold;">출처 | </span> {{ postData.authorNickname }}의 블로그</p>
+        </div>
         <hr>
         <div id="post-content" v-html="postData.pcontent"></div>
         <hr>
@@ -82,6 +89,7 @@ export default {
     return {
       liked: null,
       dialog: false,
+      time: ""
     }
   },
   components: {
@@ -118,11 +126,47 @@ export default {
     },
     closeModal(flag) {
       this.dialog = flag
+    },
+    timeBefore(){
+
+      const now = new Date();
+      const origin = new Date(this.postData.postTime);
+      if(now.getDate() == origin.getDate()){
+        const nowTime = now.getTime();
+        const originTime = origin.getTime();
+          console.log("여기")
+        if(nowTime > originTime){
+          let sec = parseInt(nowTime - originTime) / 1000;
+          let day = parseInt(sec/60/60/24);
+          sec = (sec - day*60*60*24);
+          let hour = parseInt(sec/60/60);
+          sec = (sec - (hour*60*60));
+          let min = parseInt(sec/60);
+          sec = parseInt(sec-(min*60));
+          if(hour>0){
+          //몇시간전인지
+            console.log(hour+"시간 전");
+            this.time = hour+"시간 전";
+          }else if(min>0){
+          //몇분전인지
+            console.log(min+"분 전");
+            this.time = min+"분 전";
+          }else if(sec>0){
+          //몇초전인지 계산
+              // document.getElementsByClassName("sub")[0].innerHTML = sec+"초 전";
+            console.log(sec+"초 전");
+            this.time = sec+"초 전";
+          }
+        }
+      }else {
+        this.time = this.postData.postTime;
+      }
     }
   },
   async created() {
     const { data } = await axios.get(`${process.env.VUE_APP_SERVER}/posts/${this.postData.pid}/likes`,{headers: {"auth": cookies.get('auth-token')}})
     this.liked = data.data
+    this.timeBefore()
   }
 }
 </script>
