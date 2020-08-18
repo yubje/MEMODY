8ecc273d275a37d91543ca00e39a17785965c034
김재영 <gjr191@naver.com>
2020-08-18 20:56:58 +0900

    S03P13A306-125|Done |Blog Hotfix

--- frontend/src/views/BlogView.vue
+++ frontend/src/views/BlogView.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-    <BlogPostSidebar/>
-    <div class="col align-self-center" style="height: 60%;">
-      <div class="col text-left m-5">
-        <h1 class="font-weight-black mb-3">{{ blogData.btitle }}</h1>
-        <h3 class="font-weight-bold ">{{ blogData.bsubtitle }}</h3>
-        
-        <p>{{ blogData.bcontent }}</p>
-        
-        <p>관리자 {{ blogData.manager }}</p>
-        <v-btn
-          outlined
-          rounded
-          color="teal"
-          class="ma-1"
-          v-for="hashtag in blogData.hashtags"
-          :key="hashtag.tname"
-        >
-          <v-icon>mdi-music-accidental-sharp</v-icon>
-          {{ hashtag.tname }}
-        </v-btn>
-        <p><span class="font-weight-bold">팔로워</span> {{blogData.followers}} 명</p>
-        
-        <!-- 블로그 관리자가 아닌 경우 -->
-        <div v-if="userInfo.email !== blogData.manager">
-          <!-- 블로그 회원인 경우 블로그 탈퇴 버튼 보여주기 -->
-          <div v-if="this.isMember">
-            <!-- <v-btn 
+      <BlogPostSidebar />
+      <div class="col align-self-center" style="height: 60%;">
+        <div class="col text-left m-5">
+          <h1 class="font-weight-black mb-3">{{ blogData.btitle }}</h1>
+          <h3 class="font-weight-bold ">{{ blogData.bsubtitle }}</h3>
+
+          <p>{{ blogData.bcontent }}</p>
+
+          <p>관리자 {{ blogData.manager }}</p>
+          <v-btn outlined rounded color="teal" class="ma-1" v-for="hashtag in blogData.hashtags" :key="hashtag.tname">
+            <v-icon>mdi-music-accidental-sharp</v-icon>
+            {{ hashtag.tname }}
+          </v-btn>
+          <p><span class="font-weight-bold">팔로워</span> {{blogData.followers}} 명</p>
+
+          <!-- 블로그 관리자가 아닌 경우 -->
+          <div v-if="userInfo.email !== blogData.manager">
+            <!-- 블로그 회원인 경우 블로그 탈퇴 버튼 보여주기 -->
+            <div v-if="this.isMember">
+              <!-- <v-btn 
               v-if="this.isMember"
               color="error" 
               data-toggle="modal" 
@@ -37,35 +30,21 @@
               dark
               fab
             ><v-icon dark>mdi-account-remove-outline</v-icon></v-btn> -->
-          </div>
-          <!-- 블로그 회원이 아닌 경우 -->
-          <div v-else>
-            <!-- 팔로우 하고 있는 경우 -->
-            <v-btn 
-              color="secondary" 
-              small
-              dark
-              fab
-              v-if="following"
-              @click="clickFollow()"
-            >
-              <v-icon dark>mdi-account-remove-outline</v-icon>
-            </v-btn>
+            </div>
+            <!-- 블로그 회원이 아닌 경우 -->
+            <div v-else>
+              <!-- 팔로우 하고 있는 경우 -->
+              <v-btn color="secondary" small dark fab v-if="following" @click="clickFollow()">
+                <v-icon dark>mdi-account-remove-outline</v-icon>
+              </v-btn>
 
-            <v-btn 
-              color="teal" 
-              small
-              dark
-              fab
-              v-else
-              @click="clickFollow()"
-            >
-              <v-icon>mdi-account-star-outline</v-icon>
-          </v-btn>
-          </div>
-          
-            
-          <!-- <div v-for="(member, n) in members" :key="n">
+              <v-btn color="teal" small dark fab v-else @click="clickFollow()">
+                <v-icon>mdi-account-star-outline</v-icon>
+              </v-btn>
+            </div>
+
+
+            <!-- <div v-for="(member, n) in members" :key="n">
             <v-btn 
               v-if="userInfo.email==member.email"
               color="error" 
@@ -78,99 +57,115 @@
             ><v-icon dark>mdi-account-remove-outline</v-icon></v-btn>
           </div> -->
 
-          <!-- Modal -->
-          <div class="modal fade" id="leaveBlogModal" tabindex="-1" role="dialog" aria-labelledby="leaveBlogModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="leaveBlogModalLabel">블로그 탈퇴</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  블로그 회원 탈퇴를 하시겠습니까?
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">취소</button>
-                  <button type="button" class="btn btn-danger" @click="leaveBlog(userInfo.email)">탈퇴하기</button>
+            <!-- Modal -->
+            <div class="modal fade" id="leaveBlogModal" tabindex="-1" role="dialog"
+              aria-labelledby="leaveBlogModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="leaveBlogModalLabel">블로그 탈퇴</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    블로그 회원 탈퇴를 하시겠습니까?
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">취소</button>
+                    <button type="button" class="btn btn-danger" @click="leaveBlog(userInfo.email)">탈퇴하기</button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-        </div>           
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-import BlogPostSidebar from '@/components/blog/sidebar/BlogPostSidebar.vue'
+  import {
+    mapState,
+    mapActions
+  } from 'vuex'
+  import BlogPostSidebar from '@/components/blog/sidebar/BlogPostSidebar.vue'
 
-import axios from 'axios'
-import cookies from 'vue-cookies'
+  import axios from 'axios'
+  import cookies from 'vue-cookies'
 
-export default {
-  name: 'BlogView',
-  components: {
-    BlogPostSidebar,
-  },
-  data() {
-    return {
-      following: null,
-      isMember: false,
-    }
-  },
-  methods: {
-    ...mapActions('blog', ['getBlogMembers', 'deleteBlogMember']),
-    clickFollow() {
-      if (this.following) {
-        axios.delete(`${process.env.VUE_APP_SERVER}/blogs/follows`,{data :this.blogData,headers: {"auth": cookies.get('auth-token')}})
-        this.following = false
-      }else {
-        axios.post(`${process.env.VUE_APP_SERVER}/blogs/follows`,this.blogData,{headers: {"auth": cookies.get('auth-token')}})
-        this.following = true
+  export default {
+    name: 'BlogView',
+    components: {
+      BlogPostSidebar,
+    },
+    data() {
+      return {
+        following: null,
+        isMember: false,
       }
     },
-    leaveBlog(email) {
-      this.deleteBlogMember(email)
+    methods: {
+      ...mapActions('blog', ['getBlogMembers', 'deleteBlogMember']),
+      clickFollow() {
+        if (this.following) {
+          axios.delete(`${process.env.VUE_APP_SERVER}/blogs/follows`, {
+            data: this.blogData,
+            headers: {
+              "auth": cookies.get('auth-token')
+            }
+          })
+          this.following = false
+        } else {
+          axios.post(`${process.env.VUE_APP_SERVER}/blogs/follows`, this.blogData, {
+            headers: {
+              "auth": cookies.get('auth-token')
+            }
+          })
+          this.following = true
+        }
+      },
+      leaveBlog(email) {
+        this.deleteBlogMember(email)
+      },
     },
-  },
-  computed: {
-    ...mapState('blog', ['bid', 'blogData', 'members']),
-    ...mapState(['userInfo'])
-  },
-  async mounted() {
-    const { data } = await axios.get(`${process.env.VUE_APP_SERVER}/blogs/${this.blogData.bid}/follows`,{headers: {"auth": cookies.get('auth-token')}})
-    this.following = data.data
-    console.log(this.following)
-  },
-  created() {
-    this.getBlogMembers()
-    this.members.forEach(member => {
-      if (member.email === this.userInfo.email) {
-        this.isMember=true;
-      }
-    });
-  },  
-}
+    computed: {
+      ...mapState('blog', ['bid', 'blogData', 'members']),
+      ...mapState(['userInfo'])
+    },
+    async mounted() {
+      const { data
+      } = await axios.get(`${process.env.VUE_APP_SERVER}/blogs/${this.blogData.bid}/follows`, {
+        headers: {
+          "auth": cookies.get('auth-token')
+        }
+      })
+      this.following = data.data
+      console.log(this.following)
+    },
+    created() {
+      this.getBlogMembers()
+      this.members.forEach(member => {
+        if (member.email === this.userInfo.email) {
+          this.isMember = true;
+        }
+      });
+    },
+  }
 </script>
 
 <style>
-#blog {
-  height: 100%;
-}
+  #blog {
+    height: 100%;
+  }
 
-.container-fluid {
-  height: 100%;
-}
+  .container-fluid {
+    height: 100%;
+  }
 
-.row {
-  height: 100%;
-}
-
-
+  .row {
+    height: 100%;
+  }
 </style>
