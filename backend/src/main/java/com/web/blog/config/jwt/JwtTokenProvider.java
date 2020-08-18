b9b57ae63006caea238d8452abbb0ef2ec7c1a13
TAEK <hyungtaik@naver.com>
2020-08-18 21:21:42 +0900

    S03P13A306-116 | Done | JWT & Filter Update

--- backend/src/main/java/com/web/blog/config/jwt/JwtTokenProvider.java
+++ backend/src/main/java/com/web/blog/config/jwt/JwtTokenProvider.java
@@ -68,7 +68,7 @@ public class JwtTokenProvider {
 		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
 	}
 
-	// Request의 Header에서 token 값을 가져옵니다. "X-AUTH-TOKEN" : "TOKEN값'
+	// Request의 Header에서 token 값을 가져옵니다. "auth" : "TOKEN값'
 	public String resolveToken(HttpServletRequest request) {
 		return request.getHeader("auth");
 	}
