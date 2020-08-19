a4ea85eb322cf25fd2dac63aca394b4db51e85bb
현유진 <jin961221@naver.com>
2020-08-19 15:55:04 +0900

    S03P13A306-143 | In Progress | User Following Blog Design

--- frontend/src/views/main/MainFollowBlogListView.vue
+++ frontend/src/views/main/MainFollowBlogListView.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-    <h3>팔로잉 블로그</h3>
-    <div style="width:85%; margin:auto" class="container-fluid">
-      <div class="row justify-content-between">
-        <MainBlogItem class="col-md-4 mt-2" v-for="blog in followBlog" :key="blog.bid" :blog="blog" />
+  <div class="blog-list-container">
+    <div>
+      <p class="blog-list-title">팔로잉 블로그</p>
+      <p class="blog-list-subtitle">팔로우한 블로그를 볼 수 있어요</p>
+    </div>
+
+    <div class="container-fluid ">
+      <div class="row">
+        <MainBlogItem style="margin:0px !important"  class="col-md-4 mt-2" v-for="blog in followBlog" :key="blog.bid" :blog="blog" />
       </div>
-      <v-dialog v-model="dialog" persistent max-width="500">
-        <MainCreateBlog @closemodal="dialog=false" />
-      </v-dialog>
     </div>
   </div>
 </template>
