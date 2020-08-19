ab13bec56caa76555280c206f91781594e6f0c97
jkim0715 <gjr191@naver.com>
2020-08-19 14:11:04 +0900

    [MR] |2020-08-19 |

--- frontend/src/components/blog/sidebar/BlogPostSidebar.vue
+++ frontend/src/components/blog/sidebar/BlogPostSidebar.vue
@@ -1,46 +1,39 @@
 <!--카테고리 2중-->
 <template>
- <v-card
-    class="mr-auto"
-    height="100%;"
-    width="256"
-  >
-    <v-navigation-drawer
-      class="teal lighten-5"
-      absolute
-      light
-      permanent
-      right
-    >
+  <v-card class="mr-auto" height="100%;" width="256">
+    <v-navigation-drawer class="teal lighten-5" absolute light permanent right>
       <v-list>
         <v-list-item>
-          <router-link :to="{ name: 'BlogView' }" class="text-dark text-decoration-none"><h4> Home</h4></router-link>
+          <router-link :to="{ name: 'BlogView' }" class="text-dark text-decoration-none">
+            <h4> Home</h4>
+          </router-link>
         </v-list-item>
         <v-list-item>
           <router-link :to="{ name: 'BlogPostList' }" class="text-dark text-decoration-none">전체글조회</router-link>
         </v-list-item>
-        <div v-for="categories in dataCategories"
-          :key="categories.lcid" >
-          <v-list-item
-            link
-          >
+        <div v-for="categories in dataCategories" :key="categories.lcid">
+          <v-list-item link>
             <v-list-item-content>
-              <v-list-item-title><h5>{{ categories.large_dir }}</h5></v-list-item-title>
+              <v-list-item-title>
+                <h5>{{ categories.large_dir }}</h5>
+              </v-list-item-title>
             </v-list-item-content>
           </v-list-item>
 
 
           <v-list-item v-for="child in categories.mcategory" :key="child.mcid" link>
-            <v-list-item-title @click="moveToPost(child.mcid, blogData.bid, categories.lcid),fetchPost(child.mcid,blogData.bid)">
+            <v-list-item-title
+              @click="moveToPost(child.mcid, blogData.bid, categories.lcid),fetchPost(child.mcid,blogData.bid)">
               | {{child.medium_dir}}
             </v-list-item-title>
-          </v-list-item>         
+          </v-list-item>
         </div>
       </v-list>
       <template v-slot:append>
         <div class="d-flex justify-end pa-5">
           <v-icon>mdi-wrench</v-icon>
-          <router-link :to="{name: 'BlogSettingsInfo', query: {bid: blogData.bid }}" class="text-dark text-decoration-none">Settings</router-link>
+          <router-link :to="{name: 'BlogSettingsInfo', query: {bid: blogData.bid }}"
+            class="text-dark text-decoration-none">Settings</router-link>
         </div>
       </template>
     </v-navigation-drawer>
@@ -48,51 +41,55 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+  import {
+    mapState,
+    mapActions
+  } from 'vuex'
 
-export default {
-  name: 'BlogPostSidebar',
-  components: {
-    
-  },
-  props: {
-    bid: Number 
-  },
-  computed: {
-    ...mapState('blog', ['blogData','dataCategories'])
-  },
-  methods: {
-    ...mapActions('blog',['addParentCategory','addChildCategory','getBlogCategory','moveToPosts','fetchPosts']),
-    moveToPost(mcid,bid,lcid) {
-      const categoryData = {
-        "bid": bid,
-        "mcid": mcid,
-        "lcid": lcid,
-      }
-      this.moveToPosts(categoryData)
+  export default {
+    name: 'BlogPostSidebar',
+    components: {
+
     },
-    fetchPost(mcid, bid) {
-      const temp ={
-        "bid": bid,
-        "mcid": mcid
+    props: {
+      bid: Number
+    },
+    computed: {
+      ...mapState('blog', ['blogData', 'dataCategories'])
+    },
+    methods: {
+      ...mapActions('blog', ['addParentCategory', 'addChildCategory', 'getBlogCategory', 'moveToPosts', 'fetchPosts']),
+      moveToPost(mcid, bid, lcid) {
+        const categoryData = {
+          "bid": bid,
+          "mcid": mcid,
+          "lcid": lcid,
+        }
+        this.moveToPosts(categoryData)
+      },
+      fetchPost(mcid, bid) {
+        const temp = {
+          "bid": bid,
+          "mcid": mcid
+        }
+        this.fetchPosts(temp)
       }
-      this.fetchPosts(temp)
+    },
+    created() {
+      this.getBlogCategory(this.blogData.bid)
     }
-  },
-  created() {
-    this.getBlogCategory(this.blogData.bid)
   }
-}
 </script>
 
 
 <style scoped>
-p {
-  cursor: pointer;
-}
-th {
-  cursor: pointer;
-  color: #313D4F;
+  p {
+    cursor: pointer;
+  }
 
-}
+  th {
+    cursor: pointer;
+    color: #313D4F;
+
+  }
 </style>
